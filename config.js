5448ef05cdd1c930f1ab2dcb32e2c9e23003a523
Fsociety <101577336+Fsociety1197@users.noreply.github.com>
2022-03-14 18:42:13 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  databaseToken: 'mongodb+srv://Zone:Zone@cluster0.z2bwm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+
+  mainGuild: 'Guild Id',
+  logChannel: 'Log Channel',
+  mailChannel: 'Category Id',
+  modRoles: ['role id 1', 'role id 2'],
+
+  status: 'Made By The Ice',
+  color: "embed color",
+  prefix: 'prefix',
+  msgPrefix: 'Team Ice',
+};
